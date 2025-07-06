5a599dddfabe2dfed550b1f5be6a1cd65f1c890e
Wint Wah Lwin <wintwah.lwin@my.jcu.edu.au>
2025-07-06 13:44:53 +0800

    Added docstrings

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,12 +1,15 @@
 class ProgrammingLanguage:
     def __init__(self,name, typing, reflection, year):
+        """initialize programming language"""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
+        """return string representation of programming language"""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
+        """return true if programming language is dynamic"""
         return self.typing == "Dynamic"
