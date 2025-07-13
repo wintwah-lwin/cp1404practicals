42549dea45f3a6a75b4fbab10d08d09ab5a0fd29
Wint Wah Lwin <wintwah.lwin@my.jcu.edu.au>
2025-07-13 14:12:55 +0800

    Complete second part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -5,11 +5,11 @@ FILENAME = "guitars.csv"
 def main():
     guitars = load_guitars(FILENAME)
     display_guitars(guitars)
-
+    guitars += get_new_guitars()
     guitars.sort()
-
     print("\nSorting guitars:")
     display_guitars(guitars)
+    save_guitars(FILENAME, guitars)
 
 def load_guitars(filename):
     """read guitar csv and return a list of Guitar objects"""
@@ -26,4 +26,23 @@ def display_guitars(guitars):
     for i, guitar in enumerate(guitars, 1):
         print(f"Guitar {i}: {guitar}")
 
-main()
+def get_new_guitars():
+    "get new guitar from user"
+    guitars = []
+    print("\nEnter your guitar:")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitars.append(Guitar(name, year, cost))
+        name = input("Name: ")
+    return guitars
+
+def save_guitars(filename, guitars):
+    with open(filename, 'w', newline="") as out_file:
+        writer = csv.writer(out_file)
+        for guitar in guitars:
+            writer.writerow([guitar.name, guitar.year, guitar.cost])
+
+if __name__ == "__main__":
+    main()
